be57434c9167428c1b1d319dfcf73f06a337c83a
jamcry <jamcry@hotmail.com>
2019-03-04 00:30:58 +0300

    Added button event listeners

--- pomodoro.js
+++ pomodoro.js
@@ -1,17 +1,22 @@
-const start = document.querySelector(".start");
-const stop = document.querySelector(".stop");
-const reset = document.querySelector(".reset");
+const startBtn = document.querySelector(".start");
+const pauseBtn = document.querySelector(".pause");
+const resetBtn = document.querySelector(".reset");
 const timerDisplay = document.querySelector(".timer");
-const status = document.querySelector(".status");
-
+const endTimeDisplay = document.querySelector(".end-time");
+const timerButtons = document.querySelectorAll("[data-mins]");
+let countdown;
+let isEuTime = false; // 24hrs(EU) or 12hrs format
+let secondsLeft = 25*60;
 
 function startTimer(seconds) {
+    clearInterval(countdown); // Reset (if any) previous timers
     const now = Date.now()
     const then = now + seconds * 1000;
+    displayEndTime(then);
     displayTimeLeft(seconds);
 
     countdown = setInterval(() => {
-        const secondsLeft = Math.round( (then - Date.now()) / 1000 );
+        secondsLeft = Math.round( (then - Date.now()) / 1000 );
         console.log(`left: ${secondsLeft}`);
         if (secondsLeft < 0) {
             clearInterval(countdown);
@@ -30,11 +35,43 @@ function displayTimeLeft(sec) {
     timerDisplay.textContent = displayText;
 }
 
-stop.onclick = function() {
-    status.textContent = "Stopped.";
+function displayEndTime(timestamp) {
+    const end = new Date(timestamp);
+    const hours = end.getHours();
+    const minutes = end.getMinutes();
+    endTimeDisplay.textContent = `Countdown ends at ${(!isEuTime && hours >= 12 ? hours-12 : hours)}:${(minutes < 10) ? "0" : ""}${minutes}`;
 }
+function startTimerOnClick() {
+    const minutes = parseInt(this.dataset.mins);
+    const seconds = minutes * 60;
+    startTimer(seconds);
+}
+timerButtons.forEach(button => button.addEventListener("click", startTimerOnClick))
 
-reset.onclick = function() {
-    timerDisplay.textContent = "00:00";
-    status.textContent = "Resetted.";
+// The following should be optimized.
+// secondsLeft cannot be const. So, it is declared outside the function.
+let pausedTimeLeft;
+startBtn.addEventListener("click", continueTimer);
+pauseBtn.addEventListener("click", pauseTimer);
+resetBtn.addEventListener("click", resetTimer);
+function pauseTimer(e){
+    console.log("paused");
+    e.preventDefault();
+    clearInterval(countdown);
+}
+function continueTimer(e){
+    console.log("continued");
+    e.preventDefault();
+    startTimer(secondsLeft);
+    end = (Date.now() + secondsLeft*1000);
+    displayEndTime(end);
+}
+function resetTimer(e){
+    console.log("resetted");
+    e.preventDefault();
+    clearInterval(countdown);
+    secondsLeft = 25*60;
+    displayTimeLeft(secondsLeft);
+    endTimeDisplay.textContent = "";
+    
 }
